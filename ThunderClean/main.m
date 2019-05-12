a20440b98d6862387da83ca544f65d41233e645f
He55 <luck.he55@gmail.com>
2019-05-12 23:46:10 +0800

    Update main.m

--- ThunderClean/main.m
+++ ThunderClean/main.m
@@ -10,8 +10,57 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+        NSString *thunderPath = @"/Applications/Thunder.app";
+        NSString *thunderPlugInsPath = @"/Applications/Thunder.app/Contents/PlugIns";
+        NSArray<NSString *> *thunderPlugIns = @[
+            // @"Thunder Extension.appex",
+            @"activitycenter.xlplugin",
+            @"advertising.xlplugin",
+            // @"applications.xlplugin",
+            // @"browserhelper.xlplugin",
+            // @"details.xlplugin",
+            @"featuredpage.xlplugin",
+            @"feedback.xlplugin",
+            @"iOSThunder.xlplugin",
+            @"livestream.xlplugin",
+            // @"liveupdate.xlplugin",
+            @"lixianspace.xlplugin",
+            @"myvip.xlplugin",
+            // @"preferences.xlplugin",
+            // @"searchtask.xlplugin",
+            @"softmanager.xlplugin",
+            @"subtitle.xlplugin",
+            @"thunderword.xlplugin",
+            @"userlogin.xlplugin",
+            @"viprenew.xlplugin",
+            @"viptask.xlplugin",
+            @"viptips.xlplugin",
+            @"xiazaibao.xlplugin",
+            @"xlbrowser.xlplugin",
+            @"xlplayer.xlplugin"];
+        
+        if (![fileManager fileExistsAtPath:thunderPath]) {
+            NSLog(@"没有找到迅雷。");
+            return 0;
+        }
+        
+        for (NSString *plugIns in thunderPlugIns) {
+            NSString *plugInsPath = [thunderPlugInsPath stringByAppendingPathComponent:plugIns];
+            if (![fileManager fileExistsAtPath:plugInsPath]) {
+                NSLog(@"\"%@\" 已删除。", plugInsPath);
+                continue;
+            }
+            
+            NSError *err = nil;
+            BOOL result = [fileManager trashItemAtURL:[NSURL fileURLWithPath:plugInsPath] resultingItemURL:nil error:&err];
+            
+            if (err || !result) {
+                NSLog(@"\"%@\" 删除失败。", plugInsPath);
+            }
+            NSLog(@"\"%@\" 删除成功。", plugInsPath);
+        }
+        NSLog(@"完成。");
     }
     return 0;
 }
