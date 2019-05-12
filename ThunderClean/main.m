20a23a530e4845273fa612f835a587e4e6dc242a
He55 <luck.he55@gmail.com>
2019-05-12 22:28:19 +0800

    Initial commit

---
+++ ThunderClean/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ThunderClean
+//
+//  Created by He55 on 2019/5/12.
+//  Copyright © 2019 He55. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
